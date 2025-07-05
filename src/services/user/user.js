5ae77367a9f6a79b72598632a40d7cfbfdfce360
mfadhillahakbar <muhammad.fadhillah@barangkulakan.com>
2025-07-05 19:38:54 +0700

    feat: enable authentication in user service hooks

--- src/services/user/user.js
+++ src/services/user/user.js
@@ -31,7 +31,7 @@ export const user = app => {
   app.service(userPath).hooks({
     around: {
       all: [
-        // authenticate('jwt'),
+        authenticate('jwt'),
         schemaHooks.resolveExternal(userExternalResolver),
         schemaHooks.resolveResult(userResolver)
       ]
